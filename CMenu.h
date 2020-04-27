70cc8d4f2567690457835659e33b658791bd0e99
scagliaa <64355082+scagliaa@users.noreply.github.com>
2020-04-27 13:47:21 +0200

    Revert "Ported 1000base to Team Fortress 2."
    This reverts commit f8d2c0b56c2f778214f40cec6f51cc8b6dc75209.

--- CMenu.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class CMenu {
-public:
-	void Menu();
-private:
-
-}; extern CMenu* g_Menu;
--- Misc.cpp
+++
@@ -1,9 +0,0 @@
-#include "Main.h"
-
-Globals* g_Globals = new Globals();
-Utils* g_Utils = new Utils();
-Interfaces* g_Interfaces = new Interfaces();
-ImGuiRendering* g_ImRender = new ImGuiRendering();
-Visuals* g_Visuals = new Visuals();
-CMenu* g_Menu = new CMenu();
-CVars* g_Vars = new CVars();
