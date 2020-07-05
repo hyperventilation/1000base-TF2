171ee21d371875247694a53559576039fd9ec162
scagliaa <64355082+scagliaa@users.noreply.github.com>
2020-07-05 15:28:10 +0200

    Bruteforcing VEngineClient
    VEngineClient is 014 on TF2Classic.

--- Interfaces.cpp
+++ Interfaces.cpp
@@ -13,14 +13,14 @@ IPlayerInfoManager* I::PlayerInfo = nullptr;
 void Interfaces::InitInterfaces()
 {
 	I::Client = g_Interfaces->get_interface<IBaseClientDLL*>("client.dll", "VClient017");
-	I::Engine = g_Interfaces->get_interface<IVEngineClient*>("engine.dll", "VEngineClient013");
+	I::Engine = (IVEngineClient *)g_Interfaces->brute_iface("EngineClient", "VEngineClient", "g_pEngine", "engine.dll");
 	I::EntityList = g_Interfaces->get_interface<IClientEntityList*>("client.dll", "VClientEntityList003");
 	I::Panels = g_Interfaces->get_interface<Panel*>("vgui2.dll", "VGUI_Panel009");
 	I::Surface = g_Interfaces->get_interface<ISurface*>("vguimatsurface.dll", "VGUI_Surface030");
 	I::DebugOverlay = g_Interfaces->get_interface<IVDebugOverlay*>("engine.dll", "VDebugOverlay003");
 	I::PlayerInfo = g_Interfaces->get_interface<IPlayerInfoManager*>("server.dll", "PlayerInfoManager002");
 
-	DWORD dwClientModeAddress = g_Utils->FindPatternIDA("client.dll", "8B 0D ? ? ? ? 8B 02 D9 05");
-	I::ClientMode = **(IClientModeShared***)( dwClientModeAddress + 2);  
+	DWORD* dwCHLClientTable = (DWORD*)*(DWORD*)I::Client;
+	I::ClientMode = **(IClientModeShared***)((DWORD)dwCHLClientTable[10] + 0x05);
 	I::Globals = I::PlayerInfo->GetGlobalVars();
 }
--- Interfaces.h
+++ Interfaces.h
@@ -24,14 +24,39 @@ namespace I {
 
 class Interfaces {
 public:
+	using original_fn = void* (*)(const char*, int*);
 	template <typename t = void*>
 	t get_interface(const char* module_name, const char* interface_name) {
-		using original_fn = void*(*)(const char*, int*);
 		original_fn create_interface = (original_fn)GetProcAddress(GetModuleHandleA(module_name), "CreateInterface");
 		return reinterpret_cast< t >(create_interface(interface_name, nullptr));
 	}
+	void* brute_iface(const char* name, const char* interfacen, const char* ptr_name, const char* pinterface)
+	{
+		std::string string_interface = "";
+		std::string interface_version = "0";
 
+		for (int i = 0; i <= 99; i++)
+		{
+			string_interface = interfacen;
+			string_interface += interface_version;
+			string_interface += std::to_string(i);
+
+			original_fn create_interface = (original_fn)GetProcAddress(GetModuleHandleA(pinterface), "CreateInterface");
+
+			void* func_ptr { create_interface(string_interface.c_str(), NULL) };
+
+			if ((DWORD)func_ptr != 0x0)
+				return func_ptr;
+
+			if (i >= 99 && interface_version == "0")
+			{
+				interface_version = "00";
+				i = 0;
+			}
+			else if (i >= 99 && interface_version == "00") { printf("error"); }
+		}
+		return FALSE;
+	}
 	void InitInterfaces();
-
 private:
 }; extern Interfaces* g_Interfaces;
